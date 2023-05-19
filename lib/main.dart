25daed138641f21fd3876c356825120869f9c3e9
baifengheixi <baifeng.x.heixi@gmail.com>
2023-05-19 13:59:20 +0700

    feat(🚀): setup splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:tradix/app/app.dart';
 import 'package:tradix/app/dependency_injection/dependencies.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await dotenv.load(fileName: ".env");
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory: await getApplicationDocumentsDirectory(),
   );
   await configureDependencies();
   runApp(MyApp());
+  FlutterNativeSplash.remove();
 }
